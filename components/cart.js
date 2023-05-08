9b645aeb93a6bf35ac3c2f5c2b7cef78ed64dc68
MAERA <maera.contaret@outlook.fr>
2023-05-08 19:33:08 +0200

    test

--- components/cart.js
+++ components/cart.js
@@ -1,27 +1,100 @@
-const cartContainer = document.createElement("div")
-const cartTitle = document.createElement("h4")
-const cart = document.createElement("ul")
-const totalPrice = document.createElement("p")
-const closeIcon = document.createElement("i")
+// Création des éléments HTML pour le panier
+const cartContainer = document.createElement('div')
+const cartTitle = document.createElement('h4')
+const cartList = document.createElement('ul')
+const totalPrice = document.createElement('p')
+const closeIcon = document.createElement('i')
 
-cartTitle.textContent = "Mon panier"
-totalPrice.innerHTML = "Total:" + "<span id='total'></span>"
+// Configuration des éléments HTML pour le panier
+cartTitle.textContent = 'Mon panier'
+totalPrice.innerHTML = 'Total : <span id="total"></span> €'
+closeIcon.classList.add('fas', 'fa-times')
+cartContainer.classList.add('cart-container')
+cartList.setAttribute('id', 'cart')
 
-closeIcon.classList.add("fas", "fa-times")
-cartContainer.classList.add("cart-container")
+// Gestion de la fermeture du panier
+closeIcon.addEventListener('click', () => {
+  cartContainer.style.display = 'none'
+});
 
-cart.setAttribute("id", "cart")
+// Affichage du panier lorsque l'icône est cliquée
+cartIcon.addEventListener('click', () => {
+  displayCart();
+  cartContainer.style.display = 'block'
+});
 
-closeIcon.addEventListener("click", () => {
-    cartContainer.style.display = "none"
-})
-
-cartIcon.addEventListener("click", () => {
-    cartContainer.style.display = "block"
-})
-
-header.appendChild(cartContainer)
+// Ajout des éléments HTML à la page
 cartContainer.appendChild(cartTitle)
-cartContainer.appendChild(cart)
+cartContainer.appendChild(cartList)
 cartContainer.appendChild(totalPrice)
 cartContainer.appendChild(closeIcon)
+header.appendChild(cartContainer)
+
+
+// Affichage du contenu du panier
+function displayCart() {
+
+  //const cartList = document.querySelector('#cart')
+  const total = document.querySelector('#total')
+
+  // Réinitialisation du contenu du panier
+  cartList.innerHTML = ''
+
+  // Parcours des articles dans le panier
+  cart.forEach(item => {
+    // Création des éléments HTML pour chaque article
+    const li = document.createElement('li')
+    const name = document.createElement('span')
+    const price = document.createElement('span')
+    const removeBtn = document.createElement('button')
+
+    // Configuration des éléments HTML
+    name.textContent = item.product.name
+    price.textContent = item.product.price + ' €'
+    removeBtn.textContent = 'Supprimer'
+    removeBtn.classList.add('remove-btn')
+
+    // Suppression de l'article du panier lorsque le bouton "Supprimer" est cliqué
+    removeBtn.addEventListener('click', () => {
+      removeItemFromCart(item)
+      displayCart()
+    });
+
+    li.appendChild(name)
+    li.appendChild(price)
+    li.appendChild(removeBtn)
+    cartList.appendChild(li)
+  })
+
+  // Calcul et affichage du prix total
+  const totalPrice = cart.reduce((acc, item) => acc + item.product.price, 0)
+  total.textContent = totalPrice
+  console.log(totalPrice)
+}
+
+// Mise à jour du contenu du panier depuis le localStorage
+function loadCartFromLocalStorage() {
+  cart = JSON.parse(localStorage.getItem('cart')) || []
+  displayCart();
+}
+
+// supprime un article du panier
+function removeItemFromCart(item) {
+    // Recherche de l'index de l'article à supprimer dans le panier
+    const index = cart.findIndex(cartItem => cartItem.name === item.name)
+  
+    if (index !== -1) {
+      // Suppression de l'article du panier
+      cart.splice(index, 1)
+  
+      // Mise à jour du localStorage avec le nouveau panier
+      localStorage.setItem('cart', JSON.stringify(cart))
+    }
+  }
+
+// Appel de la fonction loadCartFromLocalStorage() lorsque la page est chargée
+window.addEventListener('load', loadCartFromLocalStorage)
+
+
+
+
--- pages/menu.js
+++ pages/menu.js
@@ -1,4 +1,114 @@
 let restaurants = []
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+fetch("../datas/restaurants.json")
+.then((res) => res.json())
+.then((data) => {
+    restaurants = data
+    
+    restaurants.forEach((restaurant) => {
+        // Vérifie si l'url contient bien l'id du restaurant 
+        let verifyUrl = new URLSearchParams(window.location.search)
+        verifyUrl.has(restaurant.id)
+        let param = verifyUrl.get("id")
+
+        // Si l'id du restaurant correspond à l'id dans l'url
+        if (restaurant.id == param) {
+            // Affiche la bannière et le titre du restaurant qui correspond 
+            displayBanner(restaurant)
+            displayTitleMenu(restaurant)
+
+            const products = restaurant.products 
+            const menuSection = document.querySelector(".menu")
+
+            // Implémentation de chaque produit dans le DOM
+            products.forEach((product) => {
+                
+                const productContainer = document.createElement("div")
+                productContainer.classList.add("bloc__container")
+
+                productContainer.innerHTML = `
+                    <div class="bloc__container__card column animation">
+                        <h4 class="bloc__container__card__title">${product.name}</h4>
+                        <div class="bloc__container__card__description">
+                            <p class="details">${product.description}</p>
+                            <span class="price">${product.price}<span>
+                        </div>
+                    </div>
+                    <div class="bloc__container__icone">
+                        <button class="btn-plus add-to-cart-btn" data-id="${product.id}" data-name="${product.name}" >
+                            <i class="fas fa-plus"></i>
+                        </button>
+                        <select id="quantity" name="q" class="quantity">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                `
+                menuSection.appendChild(productContainer)
+
+                const addToCartBtn = productContainer.querySelector(".add-to-cart-btn")
+                const quantitySelect = productContainer.querySelector(".quantity")
+
+                // Ajoute le nom du produit et la quantité dans la panier
+                addToCartBtn.addEventListener("click", (e) => {
+                    const productName = e.currentTarget.getAttribute("data-name")
+                    const quantity = parseInt(quantitySelect.value)
+                    addToCart(productName, quantity)
+                    console.log(quantity)
+                })
+            })
+
+            function addToCart(productName, quantity) {
+
+                // Recherche le produit correspondant au nom du produit dans le tableau `products`
+                const product = products.find(product => product.name === productName);
+                
+                if (!product) {
+                  console.error(`Le produit avec le nom ${productName} n'existe pas.`)
+                  return;
+                }
+              
+                // Vérification de la quantité
+                if (isNaN(quantity) || quantity <= 0) {
+                    console.error(`La quantité doit être un nombre positif.`)
+                    return;
+                }
+
+                // Recherche si le produit est déjà présent dans le panier
+                const existingProduct = cart.find(item => item.product.name === productName);
+                
+                if (existingProduct) {
+                  // Si le produit existe déjà dans le panier, on ajoute simplement la quantité
+                  existingProduct.quantity += quantity
+                } else {
+                  // Si le produit n'existe pas encore dans le panier, on l'ajoute avec la quantité
+                  cart.push({
+                    product: product,
+                    quantity: quantity
+                  })
+                }
+
+                // Stocker le panier dans le localStorage
+                localStorage.setItem('cart', JSON.stringify(cart))
+              
+                // Affichage du contenu du panier dans la console
+                //console.log(cart) 
+              
+            }
+              
+            
+        }
+    })
+})
+.catch
+
+
+/*
+let restaurants = []
 
 fetch("../datas/restaurants.json")
 .then((res) =>res.json())
@@ -88,3 +198,4 @@ fetch("../datas/restaurants.json")
     })
 })
 
+*/
